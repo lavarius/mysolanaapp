bd7e946511821fd97eae0ab6bd41e8ad4eeb3ce0
Mark Bartolo <hello@mark-bartolo.com>
2024-05-18 12:02:27 -0700

    Made structs for create, count and base accounts

--- programs/mysolanaapp/src/lib.rs
+++ programs/mysolanaapp/src/lib.rs
@@ -1,4 +1,5 @@
 use anchor_lang::prelude::*;
+use anchor_lang::solana_program::entrypoint::ProgramResult;
 
 declare_id!("5Kec3XAQJ8NC6CkRbSigxzDr67b8UEnqsURSQMsZVgve");
 
@@ -9,7 +10,41 @@ pub mod mysolanaapp {
     pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
         Ok(())
     }
+    pub fn create(ctx: Context<Create>) -> ProgramResult {
+        let base_account = &mut ctx.accounts.base_account;
+        base_account.count = 0;
+        Ok(())
+    }
+
+    pub fn increment(ctx: Context<Increment>) -> ProgramResult {
+        let base_account = &mut ctx.accounts.base_account;
+        base_account.count += 1;
+        Ok(())
+    }
 }
 
+// Transaction instructions
 #[derive(Accounts)]
 pub struct Initialize {}
+
+// Transaction instructions
+#[derive(Accounts)]
+pub struct Create<'info> {
+    #[account(init, payer = user, space = 16 + 16)]
+    pub base_account: Account<'info, BaseAccount>,
+    #[account(mut)]
+    pub user: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+// Transaction instructions
+#[derive(Accounts)]
+pub struct Increment<'info> {
+    #[account(mut)]
+    pub base_account: Account<'info, BaseAccount>,
+}
+
+// An account that goes inside a transaction instruction
+#[account]
+pub struct BaseAccount {
+    pub count: u64,
+}
